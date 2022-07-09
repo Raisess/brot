a6d1e761afb45aeddb12493c0e61040972c12db2
Raisess <danilosantana456789@gmail.com>
2022-07-09 11:46:17 -0300

    assert values on common Color and Size objects

--- src/common/Color.h
+++ src/common/Color.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <assert.h>
+
 namespace Common {
 
 class Color {
@@ -10,7 +12,12 @@ public:
   int alpha;
 
   Color(int red = 255, int green = 255, int blue = 255, int alpha = 255)
-    : red(red), green(green), blue(blue), alpha(alpha) {}
+    : red(red), green(green), blue(blue), alpha(alpha) {
+    assert(red >= 0 && red <= 255);
+    assert(blue >= 0 && blue <= 255);
+    assert(green >= 0 && green <= 255);
+    assert(alpha >= 0 && alpha <= 255);
+  }
 };
 
 }
--- src/common/Size.cpp
+++ src/common/Size.cpp
@@ -1,3 +1,7 @@
+#include <assert.h>
 #include "Size.h"
 
-Common::Size::Size(int width, int height) : width(width), height(height) {}
+Common::Size::Size(int width, int height) : width(width), height(height) {
+  assert(width >= 0);
+  assert(height >= 0);
+}
