57ddb3249ee58781beec01db7dc59a0c8198d724
Raisess <danilosantana456789@gmail.com>
2022-07-10 19:40:58 -0300

    Common: create a to_print_string method on Vec2

--- src/common/Vec2.cpp
+++ src/common/Vec2.cpp
@@ -4,6 +4,10 @@ namespace Common {
 
 Vec2::Vec2(int x, int y) : x(x), y(y) {}
 
+std::string Vec2::to_print_string() {
+  return "x: " + std::to_string(x) + ", y: " + std::to_string(y);
+}
+
 Vec2 Vec2::operator+(Vec2 vec) {
   return Vec2(x + vec.x, y + vec.y);
 }
--- src/common/Vec2.h
+++ src/common/Vec2.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 namespace Common {
 
 class Vec2 {
@@ -9,6 +11,7 @@ public:
 
   Vec2(int x, int y);
 
+  std::string to_print_string();
   Vec2 operator+(Vec2 vec);
   Vec2 operator+(int value);
   Vec2 operator*(Vec2 vec);
