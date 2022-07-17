ad589040bc812272131b434f3074615b124bf62c
Raisess <danilosantana456789@gmail.com>
2022-07-17 19:56:22 -0300

    improve frame time

--- src/gfx/Window.h
+++ src/gfx/Window.h
@@ -5,7 +5,7 @@
 #include "../common/Size.h"
 #include "SDLController.h"
 
-#define FPS_LIMIT 60
+#define FPS_LIMIT 30
 
 namespace GFX {
 
--- src/util/Time.cpp
+++ src/util/Time.cpp
@@ -1,15 +1,13 @@
 #ifdef _WIN32
 #include <windows.h>
+#define SLEEP(ms) Sleep(ms)
 #else
 #include <unistd.h>
+#define SLEEP(ms) usleep(ms * 1000)
 #endif
 
 #include "Time.h"
 
 void Util::Time::delay(unsigned int ms) {
-#ifdef _WIN32
-  Sleep(ms);
-#else
-  sleep(ms / 1000);
-#endif
+  SLEEP(ms);
 }
