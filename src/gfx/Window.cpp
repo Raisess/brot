e9caf73f8d19e683e594e4199a6994424efa360b
Raisess <danilosantana456789@gmail.com>
2022-07-24 21:44:29 -0300

    GFX::Window: improve error messages

--- src/gfx/Window.cpp
+++ src/gfx/Window.cpp
@@ -6,13 +6,13 @@ GFX::Window::Window(const std::string& title, const Common::Size& size) : title(
   Util::Logger::Debug("Create Window: " + title + " | width: " + std::to_string(size.width) + ", height: " + std::to_string(size.height));
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    Util::Logger::Error("Error initializing video " + std::string(SDL_GetError()));
+    Util::Logger::Error("Error initializing video: " + std::string(SDL_GetError()));
     exit(1);
   }
 
   sdl_value = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, size.width, size.height, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
   if (sdl_value == nullptr) {
-    Util::Logger::Error("Error creating window " + std::string(SDL_GetError()));
+    Util::Logger::Error("Error creating window: " + std::string(SDL_GetError()));
     exit(1);
   }
 }
