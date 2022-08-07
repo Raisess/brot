934ff058bc0449c3ee732c44380a95f7ee261e14
Raisess <danilosantana456789@gmail.com>
2022-08-07 20:50:25 -0300

    Util::ArgsParser: improve arguments parser algorithm

--- src/util/ArgsParser.h
+++ src/util/ArgsParser.h
@@ -1,19 +1,22 @@
 #pragma once
 
+#include <array>
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 #include "Logger.h"
 
 namespace Util {
 
-using Arguments = std::map<std::string, std::string>;
+using Arguments = std::unordered_map<std::string, std::string>;
+using Tuple = std::array<std::string, 2>;
+using PlainArguments = std::vector<std::string>;
 
 class ArgsParser {
 public:
   static Arguments Parse(int argc, char* argv[]) {
-    auto args = ArgsParser::ToStringVector(argc, argv);
-    auto map = std::map<std::string, std::string>();
+    PlainArguments args = ArgsParser::ToStringVector(argc, argv);
+    Arguments map = std::unordered_map<std::string, std::string>();
 
     if (!args.size()) {
       return map;
@@ -21,26 +24,8 @@ public:
 
     for (auto arg : args) {
       if (arg.substr(0, 2) == "--") {
-        std::string key;
-        std::string value;
-        int side = 0;
-
-        for (int i = 2; i < arg.size(); i++) {
-          std::string charc = arg.substr(i, 1);
-
-          if (charc == "=") {
-            side = 1;
-            continue;
-          }
-
-          if (side == 0) {
-            key += charc;
-          } else {
-            value += charc;
-          }
-        }
-
-        map[key] = value;
+        Tuple tuple = ArgsParser::ParseDoubleHifen(arg);
+        map[tuple[0]] = tuple[1];
       } else {
         Logger::Error("Invalid argument: " + arg);
         exit(1);
@@ -51,12 +36,29 @@ public:
   }
 
 private:
-  static std::vector<std::string> ToStringVector(int argc, char* argv[]) {
+  static Tuple ParseDoubleHifen(const std::string& arg) {
+    size_t key_size = 0;
+
+    for (size_t i = 2; i < arg.size(); i++) {
+      if (arg.substr(i, 1) == "=") {
+        break;
+      }
+
+      key_size++;
+    }
+
+    std::string key = arg.substr(2, key_size);
+    std::string value = arg.substr(key_size + 3, arg.size() - (key_size + 3));
+
+    return { key, value };
+  }
+
+  static PlainArguments ToStringVector(int argc, char* argv[]) {
     if (argc == 1) {
       return {};
     }
 
-    std::vector<std::string> vec;
+    PlainArguments vec;
 
     for (int i = 1; i < argc; i++) {
       vec.push_back(std::string(argv[i]));
