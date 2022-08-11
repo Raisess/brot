e7b1369dea519ad456b74d679125ae41342537fc
Raisess <danilosantana456789@gmail.com>
2022-08-11 19:09:43 -0300

    Input: fix test

--- src/input/test.cpp
+++ src/input/test.cpp
@@ -7,21 +7,21 @@
 int main() {
   GFX::Window window("Brot Engine | Input Test", { 800, 600 });
 
-  window.loop([&]() -> void {
+  window.loop([&](int) -> void {
     Input::Keyboard::OnPressed(Input::Keyboard::ESC, [&]() -> void {
       window.quit();
     });
 
     Input::Keyboard::OnPressed(Input::Keyboard::RETURN, [&]() -> void {
-      Util::Logger::debug("pressed RETURN");
+      Util::Logger::Debug("pressed RETURN");
     });
 
     Input::Keyboard::OnPressed({ Input::Keyboard::LCTRL, Input::Keyboard::ONE }, [&]() -> void {
-      Util::Logger::debug("pressed LCTRL + ONE");
+      Util::Logger::Debug("pressed LCTRL + ONE");
     });
 
     Input::Mouse::OnClick(Input::Mouse::LEFT, [&](Common::Vec2 pos) -> void {
-      Util::Logger::debug("pressed LEFT_CLICK at " + pos.to_print_string());
+      Util::Logger::Debug("pressed LEFT_CLICK at " + pos.to_print_string());
     });
   });
 
