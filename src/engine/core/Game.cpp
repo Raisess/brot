31f46305b48146ec05eda3f0894ed86f46c18829
Raisess <danilosantana456789@gmail.com>
2022-08-19 17:42:36 -0300

    Engine::Game: pass "window-fullscreen" flag to the game context window creation

--- src/engine/core/Game.cpp
+++ src/engine/core/Game.cpp
@@ -16,7 +16,8 @@ Engine::GameContext::GameContext(const std::string& window_title, int argc, char
     Common::Size({
       Util::ArgsParser::ToInteger(args, "window-width"),
       Util::ArgsParser::ToInteger(args, "window-height"),
-    })
+    }),
+    Util::ArgsParser::ToBoolean(args, "window-fullscreen")
   );
   render_ctx = std::make_shared<GFX::Renderer>(*window_ctx);
   engine_font = std::make_shared<GFX::Font>(INTRO_FONT_PATH);
