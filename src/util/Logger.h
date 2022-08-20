0d6fc0185126eaf704a1f16110e1b03a1f433ce0
Raisess <danilosantana456789@gmail.com>
2022-08-20 18:39:48 -0300

    Util::Logger: add log labels

--- src/util/Logger.h
+++ src/util/Logger.h
@@ -8,22 +8,39 @@ namespace Util {
 
 class Logger {
 public:
+  static void Log(const std::string& label, const std::string& message) {
+    Logger::_log("LOG", label, message);
+  }
+
   static void Log(const std::string& message) {
-    Logger::_log("LOG", message);
+    Logger::_log("LOG", "", message);
   }
 
   static void Error(const std::string& message) {
-    Logger::_log("ERROR", message);
+    Logger::_log("ERROR", "", message);
+  }
+
+  static void Error(const std::string& label, const std::string& message) {
+    Logger::_log("LOG", label, message);
   }
 
   static void Debug(const std::string& message) {
     if (DEBUG_LOG) {
-      Logger::_log("DEBUG", message);
+      Logger::_log("DEBUG", "", message);
     }
   }
 
+  static void Debug(const std::string& label, const std::string& message) {
+    Logger::_log("LOG", label, message);
+  }
+
 private:
-  static void _log(const std::string& type, const std::string& message) {
+  static void _log(const std::string& type, const std::string& label, const std::string& message) {
+    if (label != "") {
+      std::cout << "> " << type << ": (" + label + ") " << message << std::endl;
+      return;
+    }
+
     std::cout << "> " << type << ": " << message << std::endl;
   }
 };
