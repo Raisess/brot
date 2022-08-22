8349126fea375dc6985941cdd475dd46f1de2917
Raisess <danilosantana456789@gmail.com>
2022-08-21 21:52:39 -0300

    example: improve pong game

--- example/pong/main.cpp
+++ example/pong/main.cpp
@@ -3,7 +3,6 @@
 #include "../../src/common/Size.h"
 #include "../../src/gfx/Text.h"
 #include "../../src/input/Keyboard.h"
-#include "../../src/util/ArgsParser.h"
 #include "../../src/util/Number.h"
 #include "../../src/engine/core/Game.h"
 #include "../../src/engine/core/Layer.h"
@@ -12,6 +11,7 @@
 #include "../../src/engine/object/Physics.h"
 #include "../../src/engine/object/UI.h"
 
+#define FONT_PATH "../../assets/fonts/Roboto/Roboto-Regular.ttf"
 #define POINT_OFFSET 50
 #define PLAYER_OFFSET 10
 #define PLAYER_VEL 15
@@ -40,12 +40,12 @@ public:
 
   unsigned int points = 0;
 
-  Player(const GameContext& game_ctx, const Size& window_size)
+  Player(const GameContext& game_ctx)
     : Entity(game_ctx, "player", nullptr),
       _initial_pos(0) {
     fill = true;
     size = { 30, 150 };
-    _initial_pos = (window_size.height / 2) - (size.height / 2);
+    _initial_pos = (game_ctx.window->get_size().height / 2) - (size.height / 2);
     position.y = _initial_pos;
   }
 
@@ -66,33 +66,18 @@ public:
     DOWN,
   };
 
-  Ball(const GameContext& game_ctx, const Size& window_size)
+  Ball(const GameContext& game_ctx)
     : Entity(game_ctx, "ball", nullptr),
       _initial_pos({
-        ((window_size.width / 2) - (size.width / 2)),
-        ((window_size.height / 2) - (size.height / 2)),
+        ((game_ctx.window->get_size().width / 2) - (size.width / 2)),
+        ((game_ctx.window->get_size().height / 2) - (size.height / 2)),
       }) {
     fill = true;
     size = { 30, 30 };
     reset();
   }
 
-  void to_left() {
-    _hside = Side::LEFT;
-  }
-
-  void to_right() {
-    _hside = Side::RIGHT;
-  }
-
-  void reset() {
-    _hside = Util::Number::Random(Side::LEFT, Side::RIGHT);
-    _vside = Util::Number::Random(Side::UP, Side::DOWN);
-    _vel = BALL_VEL;
-    position = _initial_pos;
-  }
-
-  Player::Num tick(const Size& limit) {
+  void tick(const Common::Size& limit) {
     if (_hside == Side::RIGHT) {
       position.x += _vel;
     } else {
@@ -110,13 +95,21 @@ public:
     } else if (position.y >= (limit.height - size.height)) {
       _vside = Side::UP;
     }
-    if (position.x <= 0) {
-      return Player::Num::TWO; // increase point to player 2
-    } else if (position.x >= (limit.width - size.width)) {
-      return Player::Num::ONE; // increase point to player 1
-    }
+  }
 
-    return Player::Num::NONE;
+  void to_left() {
+    _hside = Side::LEFT;
+  }
+
+  void to_right() {
+    _hside = Side::RIGHT;
+  }
+
+  void reset() {
+    _hside = Util::Number::Random(Side::LEFT, Side::RIGHT);
+    _vside = Util::Number::Random(Side::UP, Side::DOWN);
+    _vel = BALL_VEL;
+    position = _initial_pos;
   }
 
 private:
@@ -124,6 +117,47 @@ private:
   unsigned int _vside;
   unsigned int _hside;
   Vec2 _initial_pos;
+};
+
+class Pong {
+public:
+  std::shared_ptr<Player> player_one;
+  std::shared_ptr<PointUI> player_one_ui;
+  std::shared_ptr<Player> player_two;
+  std::shared_ptr<PointUI> player_two_ui;
+  std::shared_ptr<Ball> ball;
+
+  Pong(const GameContext& game_ctx, const std::shared_ptr<GFX::Font>& game_font)
+    : player_one(std::make_shared<Player>(game_ctx)),
+      player_one_ui(std::make_shared<PointUI>(game_ctx, game_font)),
+      player_two(std::make_shared<Player>(game_ctx)),
+      player_two_ui(std::make_shared<PointUI>(game_ctx, game_font)),
+      ball(std::make_shared<Ball>(game_ctx)) {}
+
+  void tick(const Size& limit) {
+    ball->tick(limit);
+
+    Physics::Collision::IsColliding(*player_one, *ball, [&]() -> void {
+      ball->to_right();
+    });
+    Physics::Collision::IsColliding(*player_two, *ball, [&]() -> void {
+      ball->to_left();
+    });
+
+    if (ball->position.x <= 0) {
+      player_two->points++;
+      player_two_ui->text = std::to_string(player_two->points);
+      player_one->reset();
+      player_two->reset();
+      ball->reset();
+    } else if (ball->position.x >= (limit.width - ball->size.width)) {
+      player_one->points++;
+      player_one_ui->text = std::to_string(player_one->points);
+      player_one->reset();
+      player_two->reset();
+      ball->reset();
+    }
+  }
 };
 
 int main(int argc, char* argv[]) {
@@ -135,66 +169,36 @@ int main(int argc, char* argv[]) {
   auto ui_layer = scene.push_layer();
   auto level_layer = scene.push_layer();
 
-  auto player_one_points = std::make_shared<PointUI>(game.ctx, game.ctx.engine_font);
-  player_one_points->position.x = POINT_OFFSET;
-  ui_layer->nodes.push_back(player_one_points);
-  auto player_two_points = std::make_shared<PointUI>(game.ctx, game.ctx.engine_font);
-  player_two_points->position.x = (window_size.width - player_two_points->size.width) - POINT_OFFSET;
-  ui_layer->nodes.push_back(player_two_points);
+  Pong pong(game.ctx, std::make_shared<GFX::Font>(FONT_PATH));
+  pong.player_one->position.x = PLAYER_OFFSET;
+  pong.player_one_ui->position.x = POINT_OFFSET;
+  pong.player_two->position.x = (window_size.width - pong.player_two->size.width) - PLAYER_OFFSET;
+  pong.player_two_ui->position.x = (window_size.width - pong.player_two_ui->size.width) - POINT_OFFSET;
 
-  auto player_one = std::make_shared<Player>(game.ctx, window_size);
-  player_one->position.x = PLAYER_OFFSET;
-  level_layer->nodes.push_back(player_one);
-  auto player_two = std::make_shared<Player>(game.ctx, window_size);
-  player_two->position.x = (window_size.width - player_two->size.width) - PLAYER_OFFSET;
-  level_layer->nodes.push_back(player_two);
-  auto ball = std::make_shared<Ball>(game.ctx, window_size);
-  level_layer->nodes.push_back(ball);
+  level_layer->nodes.push_back(pong.player_one);
+  level_layer->nodes.push_back(pong.player_two);
+  level_layer->nodes.push_back(pong.ball);
+  ui_layer->nodes.push_back(pong.player_one_ui);
+  ui_layer->nodes.push_back(pong.player_two_ui);
 
   game.loop([&](int dt) -> void {
     Input::Keyboard::OnPressed(Input::Keyboard::ESC, [&]() -> void {
       return game.end();
     });
     Input::Keyboard::OnPressed(Input::Keyboard::W, [&]() -> void {
-      player_one->position.y = std::max(player_one->position.y - PLAYER_VEL, 0);
+      pong.player_one->position.y = std::max(pong.player_one->position.y - PLAYER_VEL, 0);
     });
     Input::Keyboard::OnPressed(Input::Keyboard::S, [&]() -> void {
-      player_one->position.y = std::min(player_one->position.y + PLAYER_VEL, window_size.height - player_one->size.height);
+      pong.player_one->position.y = std::min(pong.player_one->position.y + PLAYER_VEL, window_size.height - pong.player_one->size.height);
     });
     Input::Keyboard::OnPressed(Input::Keyboard::UP, [&]() -> void {
-      player_two->position.y = std::max(player_two->position.y - PLAYER_VEL, 0);
+      pong.player_two->position.y = std::max(pong.player_two->position.y - PLAYER_VEL, 0);
     });
     Input::Keyboard::OnPressed(Input::Keyboard::DOWN, [&]() -> void {
-      player_two->position.y = std::min(player_two->position.y + PLAYER_VEL, window_size.height - player_two->size.height);
+      pong.player_two->position.y = std::min(pong.player_two->position.y + PLAYER_VEL, window_size.height - pong.player_two->size.height);
     });
 
-    Physics::Collision::IsColliding(*player_one, *ball, [&]() -> void {
-      ball->to_right();
-    });
-    Physics::Collision::IsColliding(*player_two, *ball, [&]() -> void {
-      ball->to_left();
-    });
-
-    const Player::Num to_increase_point = ball->tick(window_size);
-    switch (to_increase_point) {
-      case Player::Num::ONE:
-        player_one->points++;
-        player_one_points->text = std::to_string(player_one->points);
-        break;
-      case Player::Num::TWO:
-        player_two->points++;
-        player_two_points->text = std::to_string(player_two->points);
-        break;
-      case Player::Num::NONE:
-        break;
-    }
-
-    if (to_increase_point != Player::Num::NONE) {
-      player_one->reset();
-      player_two->reset();
-      ball->reset();
-    }
-
+    pong.tick(window_size);
     scene.update(dt);
     scene.draw();
   });
